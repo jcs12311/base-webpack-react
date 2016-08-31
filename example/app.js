24960537a20d861ce01203ba054c67f1302713e7
jcsoo <jcs12311@gmail.com>
2016-08-31 15:07:56 +0800

    fix example page

--- example/app.js
+++ example/app.js
@@ -1,2 +1,15 @@
 import './styles.scss';
-import framework from '../src/index';
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return <div>
+      Hello React!
+    </div>
+  }
+}
+
+ReactDOM.render(<App />,
+  document.querySelector('#app'));
